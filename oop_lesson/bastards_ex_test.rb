20ace540e6a51a47555c53d2a80b55f7a39c6dab
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-03-25 19:39:36 -0400

    bastards_ex_test

--- oop_lesson/bastards_ex_test.rb
+++ oop_lesson/bastards_ex_test.rb
@@ -2,8 +2,10 @@ while 1
   puts "Enter a number>>"
   begin
     num = Kernel.gets.match(/\d+/)[0]
-  rescue
-    puts "Erroneous input! Try again..."
+  rescue StandardError=>e
+    puts "Erroneous input!"
+    puts e
+    puts "\tTry again...\n"
   else  
     puts "#{num} + 1 is: #{num.to_i+1}"
   end  
