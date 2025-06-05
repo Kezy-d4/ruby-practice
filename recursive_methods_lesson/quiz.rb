64aa7a4039b498b20814828665fbb7d695b02662
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-06-05 13:33:05 -0400

    write recursive palindrome method

--- recursive_methods_lesson/quiz.rb
+++ recursive_methods_lesson/quiz.rb
@@ -2,3 +2,17 @@
 def factorial(n)
   n == 0 ? 1 : n * factorial(n-1)
 end
+
+# Define a recursive function that returns true if a string is a palindrome and 
+# false otherwise.
+def palindrome(string)
+  if string.length == 1 || string.length == 0
+    true
+  else
+    if string[0] == string[-1]
+      palindrome(string[1..-2])
+    else
+      false
+    end
+  end
+end
