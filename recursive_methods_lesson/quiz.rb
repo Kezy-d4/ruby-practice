52120bfbe5d8629a1ed545685d81919ecb60be01
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-06-05 17:11:11 -0400

    write recursive fibonacci method

--- recursive_methods_lesson/quiz.rb
+++ recursive_methods_lesson/quiz.rb
@@ -30,3 +30,16 @@ def bottles_of_beer(n)
     bottles_of_beer(n-1)
   end
 end
+
+# Define a recursive function that takes an argument n and returns the fibonacci
+# value of that position. The fibonacci sequence is 0, 1, 1, 2, 3, 5, 8, 13, 21...
+# So fib(5) should return 5 and fib(6) should return 8.
+def fib(n)
+  if n == 0
+    0
+  elsif n == 1
+    1
+  else
+    fib(n-1) + fib(n-2)
+  end
+end
