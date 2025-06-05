009c8360e44026f4dd29760c2fb59cfea39e3e3d
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-06-05 14:32:28 -0400

    write recursive bottles method

--- recursive_methods_lesson/quiz.rb
+++ recursive_methods_lesson/quiz.rb
@@ -16,3 +16,17 @@ def palindrome(string)
     end
   end
 end
+
+# Define a recursive function that takes an argument n and prints 
+# "n bottles of beer on the wall", "(n-1) bottles of beer on the wall", ..., 
+# "no more bottles of beer on the wall".
+def bottles_of_beer(n)
+  return unless n.class == Integer && n >= 0
+
+  if n == 0
+    puts "no more bottles of beer."
+  else
+    puts n == 1 ? "#{n} bottle of beer." : "#{n} bottles of beer."
+    bottles_of_beer(n-1)
+  end
+end
