91aaa75b5c3012ed8bf4df4b047582db38339483
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-06-05 22:56:21 -0400

    write recursive flatten method

--- recursive_methods_lesson/quiz.rb
+++ recursive_methods_lesson/quiz.rb
@@ -43,3 +43,16 @@ def fib(n)
     fib(n-1) + fib(n-2)
   end
 end
+
+# Define a recursive function that flattens an array. The method should convert 
+# [[1, 2], [3, 4]] to [1, 2, 3, 4] and [[1, [8, 9]], [3, 4]] to [1, 8, 9, 3, 4].
+def flatten(array, result = [])
+  array.each do |element|
+    if element.kind_of?(Array)
+      flatten(element, result)
+    else
+      result << element
+    end
+  end 
+  result
+end
